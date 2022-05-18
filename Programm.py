d5d2e1b2f312568531cfb6e9451d9006dffb458a
ShuraNezdan <av.nezdanov@gmail.com>
2022-05-18 13:46:35 +0500

    обновление в файле Programm

--- Programm.py
+++ Programm.py
@@ -4,10 +4,18 @@ import mod.main as main
 if __name__ == '__main__':
     
     # Вводимая человеком информация
-    id_user_vk = '8398337'
-    token_ya = ''
-    number_of_photos = 8
-    name_folder = 'sdfdg'
+    # id_user_vk = '8398337'
+    # token_ya = ''
+    # number_of_photos = 8
+    # name_folder = 'Фото из ВК'
+    
+    
+    id_user_vk = input('Введите id ВК: ')
+    token_ya = input('Введите токен Яндекс Диска: ')
+    number_of_photos = int(input('Введите колличество скачиваемых фотографий: '))
+    name_folder = input('Введите название папки (в случае пустой строки, именем папки будет id ВК): ')
+    
+    
     
     if name_folder == '': name_folder = id_user_vk
     
